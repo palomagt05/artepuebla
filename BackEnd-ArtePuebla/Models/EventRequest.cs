aeafb325179fbb4cec9521cd31e488fa8ec5245c
Dilan Esau Palafox Mendez <dilan.palafox.12@gmail.com>
2024-11-20 01:27:49 +0000

    Se añaden los modelos de los c#

---
+++ BackEnd-ArtePuebla/Models/EventRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace BackEnd_ArtePuebla.Models
+{
+    public class EventRequest
+    {
+        public string nameEvent {  get; set; }
+        public string typeEvent { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public string address { get; set; }
+        public string place { get; set; }
+        public int cost { get; set; }
+        public string publicType {  get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ BackEnd-ArtePuebla/Models/UserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BackEnd_ArtePuebla.Models
+{
+    public class UserRequest
+    {
+        public string Email { get; set; }
+        public string Pass { get; set; }
+    }
+}
